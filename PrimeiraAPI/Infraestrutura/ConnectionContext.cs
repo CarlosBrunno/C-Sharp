29cfac4d65ac0476a66d59ab00872da5d973fcc9
carlosbrunno <carlos.brunno97@gmail.com>
2023-05-23 17:16:16 -0300

    Basic Employee API POST/GET

---
+++ PrimeiraAPI/Infraestrutura/ConnectionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeiraAPI.Model;
+
+namespace PrimeiraAPI.Infraestrutura
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql(
+                "Server=localhost;" +
+                "Port=5432;Database=postgres;" +
+                "User Id=postgres;" +
+                "Password=12341234;"
+                );
+       
+    }
+}
